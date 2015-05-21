9ba34c0eb5339559fe27acc60000b37e1379fb1d
jules <julesgoullee@gmail.com>
2015-05-21 08:23:53 +0200

    opacity restore element right menu config

--- public/services/graphConfig.js
+++ public/services/graphConfig.js
@@ -3,7 +3,17 @@
  */
 "use strict";
 angularApp.service("graphConfig",function(){
+    var _callbackHiddenNodes = [];
+
     return{
-        gravity:{value: 0.1 }
+        gravity:{value: 0.1 },
+        restoreHiddenNodes : function(){
+            for( var i = 0; i < _callbackHiddenNodes.length; i++ ){
+                _callbackHiddenNodes[i]();
+            }
+        },
+        onRestoreHiddenNodes : function( callback ){
+            _callbackHiddenNodes.push( callback );
+        }
     }
 });
