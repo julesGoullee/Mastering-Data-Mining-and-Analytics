7aa6d98be17cdbca1e20cb56172089ede14e660f
jules <julesgoullee@gmail.com>
2015-05-25 02:34:37 +0200

    ignore account file setting

--- modules/twitter/twitterConnector.js
+++ modules/twitter/twitterConnector.js
@@ -3,9 +3,8 @@
 var twitter = require('twitter');
 
 var config = require('../../config/config.js');
-
-
-
+var jf = require('jsonfile');
+var accounts = jf.readFileSync( __dirname + "/../../config/account.json");
 
 module.exports = {
     client : function(){
@@ -13,8 +12,8 @@ module.exports = {
     },
     onData: function( twitterKeyWord, callback ){
         var client = new twitter({
-            consumer_key: "7ZkuNkslxLXJReFnZXQiQQYmi",
-            consumer_secret: "RaOlbMXzUAhParwL95X9VWQ4j2JEjDvctiAVhnW0LjBBsRGJbM",
+            consumer_key: accounts.TWITTER_CONSUMER_KEY,
+            consumer_secret: accounts.TWITTER_CONSUMER_SECRET,
             access_token_key: "1068441726-aJMFtZQgTLP8vljzW2XkUsPoISReoAmnXGfycYd",
             access_token_secret: "moTV1xg2RTlQe3fzENn0SeMAnfj0fP8sMNeDfONDAaEbC"
         });
