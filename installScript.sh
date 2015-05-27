cb4775c2c968c764773ed519fe0f26394932946b
pac <pakokrew@gmail.com>
2015-05-27 15:23:51 +0200

    ajout script installation

---
+++ installScript.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+## Config
+
+ES_Server=0
+ESVersion=1.5.2
+nodeVersion=0.12.4
+mongoVersion=3.0.3
+startOnBoot=false
+start=false
+
+## Script
+
+#apt-get install git -y
+#git clone https://github.com/julesGoullee/Mastering-Data-Mining-and-Analytics.git
+cd Mastering-Data-Mining-and-Analytics
+
+echo -e  "Installing essentials from package manager"
+apt-get update
+apt-get install build-essential git curl openjdk-7-jre -q -y
+
+echo -e  "\n\nInstalling NodeJS..."
+curl -L -O http://nodejs.org/dist/v0.12.4/node-v$nodeVersion.tar.gz
+tar xfz node-v$nodeVersion.tar.gz
+cd node-v$nodeVersion
+./configure
+make
+make install
+cd ..
+rm -rf node-v$nodeVersion node-v$nodeVersion.tar.gz
+
+echo -e  "\n\nInstalling MongoDB..."
+curl -O http://downloads.mongodb.org/linux/mongodb-linux-x86_64-$mongoVersion.tgz
+tar xfz mongodb-linux-x86_64-$mongoVersion.tgz
+rm -rf mongodb-linux-x86_64-$mongoVersion.tgz
+mkdir -p mongodb
+mv mongodb-linux-x86_64-$mongoVersion/* mongodb
+rm -r mongodb-linux-x86_64-$mongoVersion
+cd mongodb
+echo -e  'export PATH='`pwd`'/bin:$PATH' >> ~/.bashrc 
+mkdir -p data/db
+cd ..
+
+if [ $ES_Server = 0 ]
+then
+echo -e  "\n\nInstalling ElasticSearch..."
+curl -L -O https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-$ESVersion.deb
+dpkg -i elasticsearch-$ESVersion.deb
+rm elasticsearch-$ESVersion.deb -f
+fi
+
+echo -e "\n\nInstalling npm globals..."
+npm install -g grunt
+
+echo -e "\n\nUpdating repository."
+git pull
+npm install
+
+
+#Setting elasticSearch to boot automatically
+if [ $startOnBoot = true ] 
+then
+echo -e "\n\nSetting up auto start..."
+sudo update-rc.d elasticsearch defaults 95 10
+fi
+
+
+# Starting servers
+if [ $start = true ] 
+then
+echo -e "\n\nStarting servers..."
+sudo /etc/init.d/elasticsearch start
+mongod --dbpath mongodb/data/db&
+fi
+
+# MDMA Configuration
+echo -e "\n\nConfiguring project..."
+
+es_ip=127.0.0.1
+if [ $ES_Server != 0 ]
+then 
+es_ip=$ES_Server
+fi
+
+echo $es_ip
+echo "'use strict';
+module.exports = {
+    minOccurence: 5,
+    lang: "fr",
+    elasticSearchAddr: '"$es_ip":9200',
+    domain:'10.33.1.36',
+    port:3000
+};" > config/config.js
+
+
+
+echo -e "\n\nDone ! Everything went fine."
+
+# TODO :
+# launch mongodb at start
+# todo launch mongodb as mongodb and set permissions
