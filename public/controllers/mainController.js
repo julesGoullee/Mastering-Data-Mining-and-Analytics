235665e8a5f34741c1c0519796f2428e478fba29
jules <julesgoullee@gmail.com>
2015-06-13 18:40:33 +0200

    indentation

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -3,30 +3,34 @@
 angularApp.controller("AppCtrl", function( $scope, $rootScope, socket, $mdDialog, representation, keysWord ){
 
     $rootScope.showPopup = function(){
+
         $mdDialog.show({
             controller: "ChooseTrackController",
-            templateUrl: '../dialogs/chooseTrack.html',
+            templateUrl: "../dialogs/chooseTrack.html",
             locals:
             {
                 keywords: keysWord.get()
             }
-        }).then(function() {
+        }).then(function(){
             //fermeture popup
-        }, function() {
+        },function(){
             //error
         });
     };
 
     socket.on("representation", function( representationData ){
+
         representation.setRepresentation( representationData );
         $rootScope.$broadcast("draw");
     });
 
     socket.on("newWord", function( wordObject ){
+
         $rootScope.$broadcast("addWord", wordObject );
     });
 
     socket.on("tweetCount", function( tweetCount ){
+
         representation.tweetCount.value = tweetCount;
     });
 });
