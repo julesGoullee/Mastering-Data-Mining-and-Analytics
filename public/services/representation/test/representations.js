277090f83093aebf62fa1de4c85fa3635483b957
jules <julesgoullee@gmail.com>
2015-06-13 10:47:28 +0200

    travis test

--- public/services/representation/test/representations.js
+++ public/services/representation/test/representations.js
@@ -32,7 +32,7 @@ describe('representation', function () {
             var fatherName = "MotLvl1-1";
             var father = representation.findById(fatherName);
 
-            expect(father.sons.length).toBe(1);
+            expect(father.sons.length).toBe();
             expect(father.sons[0]).toBe(wordName);
             console.log(representation.getRepresentation())
         });
@@ -43,8 +43,8 @@ describe('representation', function () {
             var fatherName = "MotLvl1-1";
             var father = representation.findByName( fatherName );
 
-            //expect(father.sons.length).toBe(3);
-            //expect(father.sons[0]).toBe(wordName);
+            expect(father.sons.length).toBe(3);
+            expect(father.sons[0]).toBe(wordName);
         });
 
     });
