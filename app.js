444f099da943fcac230a3ce162c74957e176cf71
jules <julesgoullee@gmail.com>
2015-06-16 01:42:45 +0200

    log 404

--- app.js
+++ app.js
@@ -92,6 +92,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+    console.log(req.originalUrl);
     var err = new Error("Not Found");
     err.status = 404;
     next(err);
