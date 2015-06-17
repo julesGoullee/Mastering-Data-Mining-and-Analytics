f516ee4e048d78d735063f2f40d30b16b0040732
paco <pakokrew@gmail.com>
2015-06-17 22:53:13 +0200

    Merge branch 'master' of /home/paco/Git/Mastering-Data-Mining-and-Analytics with conflicts.

--- config/config_dev.js
+++ config/config_dev.js
@@ -3,7 +3,7 @@ module.exports = {
     port: 3000,
     api : {
         active : true,
-        esAddress: '192.168.0.4:9200',
+        esAddress: '127.0.0.1:9200',
         mongo: {
             ip: "127.0.0.1",
             base: "mdma"
@@ -12,7 +12,7 @@ module.exports = {
     },
     webServer: {
         active: true,
-        apiAddress: "127.0.0.1",
-        apiPort: 3000
+        apiAddress: "109.190.95.175",
+        apiPort: 80
     }
 };
